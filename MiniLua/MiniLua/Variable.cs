fbf5ea3d752fa174112d9a3248e8e0208eecfdfd
VoidChicken <chicken.shasal@gmail.com>
2015-08-07 02:04:54 -0700

    Updated Intepreter

---
+++ MiniLua/MiniLua/Variable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniLua
+{
+    class Variable<T>
+    {
+        public string name = "";
+        public T def = default(T);
+        public bool toBool()
+        {
+            return (typeof(T) == typeof(bool)?bool.Parse(def.ToString()):false);
+        }
+    }
+}
